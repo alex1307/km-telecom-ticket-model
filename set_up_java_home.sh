cba349a73fb0359986a1b80a9acaa3295e4d279e
Ayagasha <alexander.todorov@ayagasha.com>
2020-11-10 12:04:04 +0100

    Initial commit

---
+++ set_up_java_home.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+pwd
+
+export -p JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_211.jdk/Contents/Home"
+export -p JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-15.jdk/Contents/Home"
+export -p JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-14.0.1.jdk/Contents/Home"
+
+mvn clean
